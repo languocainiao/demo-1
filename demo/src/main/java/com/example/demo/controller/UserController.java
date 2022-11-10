0d218fd994e30258fb56b435fac8aecb8c9cad93
jack <2106573829@qq.com>
2022-11-10 19:50:57 +0800

    thrid commit

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -6,7 +6,7 @@ import com.example.demo.vo.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
+
 import java.util.List;
 
 @RestController
